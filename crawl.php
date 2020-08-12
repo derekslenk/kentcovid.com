5476aadf6454ec761c4e42a6038924315ef9b82e
Derek Slenk <derek.slenk@gmail.com>
2020-08-12 04:53:05 +0000

    make it all small

--- crawl.php
+++ crawl.php
@@ -1,9 +1,12 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 include './datum.php';
 
-$BASE_URL = 'http://micovid-data.s3.us-east-2.amazonaws.com/';
-$DOWNLOAD_URL = 'http://micovid-data.s3-website.us-east-2.amazonaws.com/';
+
+$BASE_URL = 'https://micovid-data.s3.us-east-2.amazonaws.com/';
+// $DOWNLOAD_URL = 'http://micovid-data.s3-website.us-east-2.amazonaws.com/';
 
 
 
@@ -20,11 +23,17 @@ foreach ($xml->children() as $files) {
     }
 }
 
+print_r($fileList);
+
+system('aws s3 cp s3://micovid-data/kent/ kent --recursive');
+
 foreach ($fileList as $file) {
-    $data = file_get_contents($DOWNLOAD_URL . $file);
+    $data = file_get_contents($file, false);
     $json_data = json_decode($data, true);
+    print_r($json_data);
     $datum = new Datum($json_data["submitted"], $json_data["negative"], $json_data["positive"], $json_data["pending"], $json_data["deaths"], $json_data["created_at"]);
     $final_data[$json_data["created_at"]] = $datum;
+#    sleep(1);
 }
 
 $fh = fopen('/var/www/html/data.json', 'w') or die("Error opening output file");
