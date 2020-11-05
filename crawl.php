da96c37deba22559ff6d5d622508b5d3d587f83c
Derek Slenk <derek.slenk@gmail.com>
2020-11-05 21:20:35 +0000

    Changed it back to sync...

--- crawl.php
+++ crawl.php
@@ -23,21 +23,18 @@ foreach ($xml->children() as $files) {
     }
 }
 
-// print_r($fileList);
+system('aws s3 sync s3://micovid-data/kent/ kent');
 
-system('aws s3 cp s3://micovid-data/kent/ kent --recursive');
 
 foreach ($fileList as $file) {
     $data = file_get_contents($file, false);
     $json_data = json_decode($data, true);
-    // print_r($json_data);
     $datum = new Datum($json_data["submitted"], $json_data["negative"], $json_data["positive"], $json_data["pending"], $json_data["deaths"], $json_data["created_at"]);
     $final_data[$json_data["created_at"]] = $datum;
-    #    sleep(1);
 }
 
 $fh = fopen('/var/www/html/data.json', 'w') or die("Error opening output file");
 fwrite($fh, json_encode($final_data, JSON_UNESCAPED_UNICODE));
 fclose($fh);
 
-print("Crawl finished");
+print("Crawl finished\n");
